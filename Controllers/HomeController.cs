1e9eaa9857769b2c3f7058ff41ce15fb22346e3e
venka <venka@NIKHIL_CHV>
2024-10-28 15:39:28 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Mvc;
+using SurveyPlatform.Models;
+using System.Diagnostics;
+
+namespace SurveyPlatform.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult KnowMore()
+        {
+            return View();
+        }
+
+        public IActionResult SubmitResponse()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
